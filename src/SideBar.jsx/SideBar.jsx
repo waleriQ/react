4af210232642314041fb421ee172cf039184f2a8
waleiQ <77641315+waleiQ@users.noreply.github.com>
2021-11-19 00:49:41 +0600

    add file

---
+++ src/SideBar.jsx/SideBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+let SideBar = () => {
+return (
+<aside>
+<div className="sidebar">
+  <div className="new__feeds">
+  <div className="logo">
+     <img className="logo__img" src="https://lh3.googleusercontent.com/proxy/ReqGAdZ1H_7GBmV-MQ2_o9EZjwvVaukuSevKKEINTj5RqT7l-AfCgr--pmmAhmBKxcJrO2vI3DtlJtB7KofjX7qEfsWS73jaF9p5pdTsZlGlzUU5" alt="" />
+   </div>
+  </div>
+
+</div>
+</aside>
+
+
+)
+};
+
+export default SideBar; 
