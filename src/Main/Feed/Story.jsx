f100f4ac7bec020741ada1159df35a27fa959c3e
waleiQ <77641315+waleiQ@users.noreply.github.com>
2021-11-20 02:29:19 +0600

    begin do stories

--- src/Main/Feed/Story.jsx
+++ src/Main/Feed/Story.jsx
@@ -2,8 +2,56 @@ import React from 'react';
 import './Story.css';
 let Story = () => {
 return (
-<div className="story">
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, sint?
+<div className="stories_wrapper">
+  <div className="stories">
+  <div className="stories__item">
+    <a href=""><img src="https://new-magazine.ru/wp-content/uploads/2020/06/image-08-06-20-01-35-3.jpeg" alt="" className="stories__pic" /></a>
+    <div className="stories__avatar">
+      <a href="">
+        <img src="" alt="" />
+      </a>
+    </div>
+    <div className="stories__author"></div>
+  </div>
+  <div className="stories__item">
+    <a href=""><img src="https://vokrug.tv/pic/news/8/a/3/6/8a36d69bdb48be971321361eee019e55.jpg" alt="" className="stories__pic" /></a>
+    <div className="stories__avatar">
+      <a href="">
+        <img src="" alt="" />
+      </a>
+    </div>
+    <div className="stories__author"></div>
+  </div>
+  <div className="stories__item">
+    <a href=""><img src="https://n1s1.starhit.ru/b3/ca/97/b3ca976710f6004f2665dbef7f6dfb89/460x459_0_a06cb39777d549ceb47eb1ce4045992b@489x488_0x0a330c9a_7876398331538906287.jpeg" alt="" className="stories__pic" /></a>
+    <div className="stories__avatar">
+      <a href=""><img src="https://lh3.googleusercontent.com/proxy/Q51Rzgw_Jy6ozJ2-7PzLrQdEYv4PMe4wF-gMgtx6egTN7juoZsSfrIl8dzVtUCTD" alt="" />
+      </a>
+    </div>
+    <div className="stories__author"></div>
+  </div>
+  <div className="stories__item">
+    <a href=""><img src="https://st.kp.yandex.net/images/kadr/sm_3053486.jpg" alt="" className="stories__pic" /></a>
+    <div className="stories__avatar">
+      <a href="">
+        <img src="" alt="" />
+      </a>
+    </div>
+    <div className="stories__author"></div>
+  </div>
+  <div className="stories__item">
+    <a href="">
+    <img src="https://wikicomics.ru/uploads/posts/2020-08/1598387524_rayan-reynolds.jpg" alt="" className="stories__pic" />
+    </a>
+    <div className="stories__avatar">
+      <a href="">
+        <img src="" alt="" />
+      </a>
+    </div>
+    <div className="stories__author"></div>
+  </div>
+  
+  </div>
 </div>
 )
 };
