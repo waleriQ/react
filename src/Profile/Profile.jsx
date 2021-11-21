c7f3507ee818902c7f548569d04253c0c48a9b6a
waleiQ <77641315+waleiQ@users.noreply.github.com>
2021-11-21 15:00:33 +0600

    решил проблему с position fixed, начал изуть router v6

--- src/Profile/Profile.jsx
+++ src/Profile/Profile.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Profile.css';
 let Profile = () => {
 return (
-<div className="lol">
+<div className="wrapper">
   Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero repudiandae praesentium quod, ipsam, aliquid laborum voluptate earum porro obcaecati voluptatum soluta, ab ut! Dignissimos autem veritatis velit magni cum ab deserunt adipisci. Distinctio fugiat dolore, id culpa doloremque commodi illum voluptatum pariatur velit beatae esse qui alias minus? Tempora, totam doloribus, quo ex possimus magni praesentium assumenda consequuntur rerum enim nesciunt sit autem? Iusto delectus saepe aut maxime? Quas veritatis ullam cupiditate enim temporibus, quidem inventore iusto quo perspiciatis nostrum modi, id similique? Neque tempora culpa tempore magni alias, consequatur exercitationem reprehenderit, quasi ipsam ex itaque excepturi quod obcaecati sapiente.
 </div>
 
